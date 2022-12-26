b9a1db3a1c34df97772f98c67480c9bcabdade42
HYUNBIN_CHO <bluesky3268@gmail.com>
2022-12-26 21:21:09 +0900

    first commit

---
+++ src/main/java/hello/proxy/ProxyApplication.java
@@ -0,0 +1,22 @@
+package hello.proxy;
+
+import hello.proxy.config.AppV1Config;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+
+/**
+ * 예제의 3가지 상황
+ * V1 : 인터페이스와 구현클래스를 스프링 빈으로 수동 등록
+ * V2 : 인터페이스가 없는 구현체 클래스를 스프링 빈으로 수동 등록
+ * V3 : 컴포넌트 스캔으로 스프링 빈 자동 등록
+ */
+@Import(AppV1Config.class)
+@SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
+public class ProxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProxyApplication.class, args);
+	}
+
+}
---
+++ src/main/java/hello/proxy/app/v1/OrderControllerV1Impl.java
@@ -0,0 +1,31 @@
+package hello.proxy.app.v1;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * 예제의 3가지 상황
+ * V1 : 인터페이스와 구현클래스를 스프링 빈으로 수동 등록
+ * V2 : 인터페이스가 없는 구현체 클래스를 스프링 빈으로 수동 등록
+ * V3 : 컴포넌트 스캔으로 스프링 빈 자동 등록
+ */
+@Slf4j
+public class OrderControllerV1Impl implements OrderControllerV1{
+
+    private final OrderServiceV1 orderService;
+
+    public OrderControllerV1Impl(OrderServiceV1 orderService) {
+        this.orderService = orderService;
+    }
+
+    @Override
+    public String request(String itemId) {
+        orderService.orderItem(itemId);
+        log.info("itemId : {}", itemId);
+        return "OK";
+    }
+
+    @Override
+    public String nolog() {
+        return "NO LOG OK";
+    }
+}
