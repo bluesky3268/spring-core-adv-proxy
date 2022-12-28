778d7a7f0dc657b04a6748e7fb7d62688db3210c
hbcho <bluesky3268@gmail.com>
2022-12-28 10:24:58 +0900

    프록시 팩토리 - 포인트컷

--- src/test/java/hello/proxy/advisor/AdvisorTest.java
+++ src/test/java/hello/proxy/advisor/AdvisorTest.java
@@ -4,14 +4,20 @@ import hello.proxy.common.ServiceImpl;
 import hello.proxy.common.ServiceInterface;
 import hello.proxy.common.advice.TimeAdvice;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.aop.ClassFilter;
+import org.springframework.aop.MethodMatcher;
 import org.springframework.aop.Pointcut;
 import org.springframework.aop.framework.ProxyFactory;
 import org.springframework.aop.support.DefaultPointcutAdvisor;
+import org.springframework.aop.support.NameMatchMethodPointcut;
+
+import java.lang.reflect.Method;
+
 
 @Slf4j
 public class AdvisorTest {
-
 
     @Test
     void advisorTest1() {
@@ -29,4 +35,88 @@ public class AdvisorTest {
         proxy.save();
         proxy.find();
     }
+
+    @Test
+    @DisplayName("직접 만든 포인트컷")
+    void advisorTest2() {
+        ServiceInterface target = new ServiceImpl();
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+
+        DefaultPointcutAdvisor advisor = new DefaultPointcutAdvisor(new MyPointcut(), new TimeAdvice());
+        // DefaultPointcutAdvisor : Advisor인터페이스의 가장 일반적인 구현체
+        // Pointcut.True : 뭐가 오든 항상 참인 pointcut
+
+        proxyFactory.addAdvisor(advisor);
+
+        ServiceInterface proxy = (ServiceInterface) proxyFactory.getProxy();
+
+        proxy.save();
+        proxy.find();
+    }
+
+    static class MyPointcut implements Pointcut{
+        @Override
+        public ClassFilter getClassFilter() {
+            return ClassFilter.TRUE;
+        }
+
+        @Override
+        public MethodMatcher getMethodMatcher() {
+            return new MyMethodMatcher();
+        }
+    }
+
+    static class MyMethodMatcher implements MethodMatcher{
+
+        private String matchName = "save";
+
+        @Override
+        public boolean matches(Method method, Class<?> targetClass) {
+            log.info("포인트컷 호출");
+            log.info("method = {}, targetClass = {}", method.getName(), targetClass);
+            boolean result = method.getName().equals(matchName);
+            log.info("result : {}", result);
+            return result;
+        }
+
+        @Override
+        public boolean isRuntime() {
+            return false;
+        }
+
+        @Override
+        public boolean matches(Method method, Class<?> targetClass, Object... args) {
+            return false;
+        }
+    }
+
+    /**
+     * 스프링이 제공하는 포인트컷
+     * NameMatchMethodPointcut : 메서드 이름으로 매칭(PatternMatchUtils 사용)
+     * JdkRegexpMethodPointcut : jdk 정규표현식으로 매칭 
+     * TruePointcut : 항상 참
+     * AnnotationMatchingPointcut : 어노테이션으로 매칭
+     * AspectJExpressionPointcut : aspectJ 표현식으로 매칭(실무에서 가장 많이 사용)
+     */
+    @Test
+    @DisplayName("스프링이 제공하는 포인트컷")
+    void advisorTest3() {
+        ServiceInterface target = new ServiceImpl();
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+
+        NameMatchMethodPointcut pointcut = new NameMatchMethodPointcut();
+        pointcut.setMappedName("save");
+
+        DefaultPointcutAdvisor advisor = new DefaultPointcutAdvisor(pointcut, new TimeAdvice());
+        // DefaultPointcutAdvisor : Advisor인터페이스의 가장 일반적인 구현체
+        // Pointcut.True : 뭐가 오든 항상 참인 pointcut
+
+        proxyFactory.addAdvisor(advisor);
+
+        ServiceInterface proxy = (ServiceInterface) proxyFactory.getProxy();
+
+        proxy.save();
+        proxy.find();
+    }
+
 }
